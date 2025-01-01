ebb09a0bd170277c93211948549ddb62d95cb8a7
MoonKnight30 <prajwalkoppad30@gmail.com>
2025-01-01 21:00:47 +0530

    approval model

--- backend/models/approval.model.js
+++ backend/models/approval.model.js
@@ -4,14 +4,17 @@ const Schema = mongoose.Schema;
 
 const eventApprovalSchema = new Schema({
   userID: { type: String, required: true }, 
-  placeOfVisit: { type: String, required: true },
-  reason: { type: String, required: true },
-  proofOfTravel: { type: String, required: false },
-  dateOfLeaving: { type: Date, required: true },
-  arrivalDate: { type: Date, required: true },
-  emergencyContact: { type: String, required: true },
+  name: { type: String, required: true },
+  designation: { type: String, required: true },
+  phoneNumber: { type: String, required: false },
+  email: { type: String, required: false },
+  dateFrom: { type: Date, required: true },
+  dateTo: { type: Date, required: true },
+  eventCategory: { type: String, required: true },
+  venue: { type: String, required: true },
+  helpRequired: { type: String, required: true },
+  description: { type: String, required: true },
   status: { type: String, default: 'Pending', enum: ['Pending', 'Approved', 'Rejected', 'Extension'] }, // Default status
-  scanned: { type: Date, default: null },
 }, {
 });
 
